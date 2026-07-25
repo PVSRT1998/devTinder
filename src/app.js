2db2f725197b7bcdcdc6d49d4b473a943a545973
Venkata Teja <pvsreerangateja1998@gmail.com>
2026-07-25 22:16:05 +0530

    exploring routes

--- src/app.js
+++ src/app.js
@@ -8,12 +8,30 @@ const port = 3000;
 //   res.send('Hello, from the server!');
 // });
 
-app.use('/', (req, res) => {
-  res.send('Hello, from the dashboard!');
+// app.use('/', (req, res) => {
+//   res.send('Hello, from the dashboard!');
+// });
+
+// app.use('/test', (req, res) => {
+//   res.send('Hello, from the server!');
+// });
+
+/* This will only handle GET call to /user**/   
+app.get('/user', (req, res) => {
+  res.send({firstName: 'John', lastName: 'Doe'});
 });
 
-app.use('/test', (req, res) => {
-  res.send('Hello, from the server!');
+app.post('/user', (req, res) => {
+    console.log(req.body);
+  res.send("Data received successfully");
+});
+
+app.delete('/user', (req, res) => {
+  res.send('Deleted user successfully');
+});
+
+app.patch('/user', (req, res) => {
+  res.send('Updated user successfully');
 });
 
 app.listen(port, () => {
