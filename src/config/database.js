f4cdcfe930220736583528d399679685d368ffca
Venkata Teja <pvsreerangateja1998@gmail.com>
2026-08-16 16:59:32 +0530

    removed dns

--- src/config/database.js
+++ src/config/database.js
@@ -1,5 +1,5 @@
-const dns = require("node:dns");
-dns.setServers(['1.1.1.1', '1.0.0.1']); // Forces Cloudflare DNS resolution
+// const dns = require("node:dns");
+// dns.setServers(['1.1.1.1', '1.0.0.1']); // Forces Cloudflare DNS resolution
 const mongoose = require('mongoose');
 
 const MONGO_URI = 'mongodb+srv://pvsreerangateja1998_db_user:GYlUglEZfkAdaYge@cluster0.z7itfp9.mongodb.net/devTinder';
