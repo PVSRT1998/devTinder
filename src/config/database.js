8205712bb96aec18322908c6ace38b6020064b12
Venkata Teja <pvsreerangateja1998@gmail.com>
2026-08-17 17:48:28 +0530

    set 1.1.1.1 as dns

--- src/config/database.js
+++ src/config/database.js
@@ -1,5 +1,5 @@
 const dns = require("node:dns");
-dns.setServers(['8.8.8.8']); // Forces Cloudflare DNS resolution
+dns.setServers(['8.8.8.8', '1.1.1.1']); // Forces Cloudflare DNS resolution
 const mongoose = require('mongoose');
 
 const MONGO_URI = 'mongodb+srv://pvsreerangateja1998_db_user:GYlUglEZfkAdaYge@cluster0.z7itfp9.mongodb.net/devTinder';
